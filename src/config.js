3a87298ad08fe8acc651f9e2579be2069de3709a
MubasharAHMED <ahmedmubasharp@gmail.com>
2023-03-05 22:13:10 +0100

    Adding migration and db config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,19 @@
-const config = {}
+import { resolve } from "node:path"
+
+const config = {
+  port: 3001,
+  db: {
+    client: "pg",
+    connection: {
+      user: "mubashar",
+      database: "rest_api",
+      password: "password",
+    },
+    migrations: {
+      directory: resolve("./src/db/migrations"),
+      stub: resolve("./src/db/migration.stub"),
+    },
+  },
+}
 
 export default config

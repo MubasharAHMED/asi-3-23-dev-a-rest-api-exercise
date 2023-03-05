f8cbe04ddfbd6821ef6da669e08857b418161228
MubasharAHMED <ahmedmubasharp@gmail.com>
2023-03-05 22:23:31 +0100

    adding the server

---
+++ src/runServer.js
@@ -0,0 +1,30 @@
+import cors from "cors"
+import express from "express"
+import knex from "knex"
+import morgan from "morgan"
+import handleError from "./middlewares/handleError.js"
+import makeRoutesUsers from "./routes/makeRoutesUsers.js"
+
+const runServer = async (config) => {
+  const app = express()
+
+  app.use(cors())
+  app.use(express.json())
+  app.use(morgan("dev"))
+
+  const db = knex(config.db)
+
+  makeRoutesUsers({ app, db })
+
+  app.use(handleError)
+
+  // handling 404: keep it always LAST!
+  app.use((req, res) => {
+    res.status(404).send({ error: [`cannot POST ${req.url}`] })
+  })
+
+  // eslint-disable-next-line no-console
+  app.listen(config.port, () => console.log(`Listening on :${config.port}`))
+}
+
+export default runServer

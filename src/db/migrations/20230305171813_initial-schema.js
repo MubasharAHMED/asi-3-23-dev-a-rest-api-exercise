a0d741489a43d3d4e88870ed1024867ec00ec864
MubasharAHMED <ahmedmubasharp@gmail.com>
2023-03-05 22:07:46 +0100

    Adding migration with knex on pg

---
+++ src/db/migrations/20230305171813_initial-schema.js
@@ -0,0 +1,14 @@
+export const up = async (knex) => {
+  await knex.schema.createTable("users", (table) => {
+    table.increments("id")
+    table.text("email").notNullable().unique()
+    table.text("firstName").notNullable()
+    table.text("lastName").notNullable()
+    table.text("password").notNullable()
+    table.date("role").notNullable()
+  })
+}
+
+export const down = async (knex) => {
+  await knex.schema.dropTable("users")
+}
